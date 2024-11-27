9d29841a5e6de91cd2b1749ae60ff25eef92946c
dcifligu <deivi.cifligu@gmail.com>
2024-11-27 14:50:41 +0100

    Update Card component to CardCarousel component

--- resources/js/Home.vue
+++ resources/js/Home.vue
@@ -5,7 +5,7 @@ import Hero from './components/Hero.vue';
 import SearchModal from './components/SearchModal.vue';
 import FooterComponent from './components/FooterComponent.vue';
 import CheckLink from './components/CheckLink.vue';
-import Card from './components/Card.vue';
+import CardCarousel from './components/CardCarousel.vue';
 
 export default {
     name: 'Home',
@@ -16,7 +16,7 @@ export default {
         SearchModal,
         FooterComponent,
         CheckLink,
-        Card
+        CardCarousel
     },
     data() {
         return {
@@ -39,24 +39,7 @@ export default {
     <div id="home" class="relative h-full scroll-smooth">
         <NavBar @toggle-search-modal="toggleSearchModal"/>
         <Hero />
-        <div class=" w-full my-16">
-          <div class="h-screen w-full mx-6">
-            <h1 class="text-4xl font-semibold my-5">
-              Latest Discoveries
-            </h1>
-            <span class="text-md font-light"> 
-              Be first to discover the latest
-            </span>
-            <div class="relative w-screen h-screen my-5">
-              <div class="absolute w-full -right-9 h-full">
-                <div class="w-full h-full flex flex-row gap-4">
-                  <Card />
-                  <Card />
-                </div>
-              </div>
-            </div>
-          </div>
-       </div>
+        <CardCarousel />
         <FooterComponent />
     </div>
 </template>
---
+++ resources/js/components/CardCarousel.vue
@@ -0,0 +1,31 @@
+<script>
+import Card from './Card.vue';
+
+export default {
+  name: 'CardCarousel',
+  components: {
+    Card
+  }
+}
+</script>
+
+<template>
+    <div class=" w-full my-16">
+        <div class="h-screen w-full mx-6">
+          <h1 class="text-4xl font-semibold my-5">
+            Latest Discoveries
+          </h1>
+          <span class="text-md font-light"> 
+            Be first to discover the latest
+          </span>
+          <div class="relative w-screen h-screen my-5">
+            <div class="absolute w-full -right-9 h-full">
+              <div class="w-full h-full flex flex-row gap-4">
+                <Card />
+                <Card />
+              </div>
+            </div>
+          </div>
+        </div>
+     </div>
+</template>
