b4ed022bf4fed3bd07a307a3d29942070ec09aee
dcifligu <deivi.cifligu@gmail.com>
2025-01-06 15:48:45 +0100

    Refactor Card and CardCarousel components

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -18,28 +18,3 @@
     </div>
 </template>
 
-<!-- <script>
-export default {
-  name: 'Card',
-  props: {
-    cardData: {
-      type: Number,
-      required: true,
-    },
-  },
-};
-</script>
-
-<template>
-  <div class="w-full h-full flex flex-col">
-    <img 
-      src="../../images/HeroBg.png" 
-      :alt="'Card ' + cardData" 
-      class="object-cover h-full w-full rounded-lg"
-    />
-    <div class="flex-col flex w-3/4 mx-auto mt-4">
-      <h1 class="text-2xl font-semibold mb-2">Card {{ cardData }}</h1>
-      <span class="text-md font-light">Be first to discover the latest</span>
-    </div>
-  </div>
-</template> -->
